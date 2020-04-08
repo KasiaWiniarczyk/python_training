e899eded3e1e7c380c347251b8e54f0304ad140b
katarzyna-winiarczyk <katarzyna.winiarczyk@xcaliber.com>
2020-04-08 22:51:46 +0200

    Parameters handover

--- test_add_group3.py
+++ test_add_group3.py
@@ -8,11 +8,12 @@ from selenium.common.exceptions import NoAlertPresentException
 import unittest, re
 from group import Group
 
+
 class TestAddGroup2(unittest.TestCase):
     def setUp(self):
         self.wd = webdriver.Firefox()
         self.wd.implicitly_wait(30)
-    
+
     def test_add_group2(self):
         wd = self.wd
         self.open_home_page(wd)
@@ -64,17 +65,22 @@ class TestAddGroup2(unittest.TestCase):
         wd.get("http://localhost:8080/addressbook/group.php")
 
     def is_element_present(self, how, what):
-        try: self.wd.find_element(by=how, value=what)
-        except NoSuchElementException as e: return False
+        try:
+            self.wd.find_element(by=how, value=what)
+        except NoSuchElementException as e:
+            return False
         return True
-    
+
     def is_alert_present(self):
-        try: self.wd.switch_to_alert()
-        except NoAlertPresentException as e: return False
+        try:
+            self.wd.switch_to_alert()
+        except NoAlertPresentException as e:
+            return False
         return True
-    
+
     def tearDown(self):
         self.wd.quit()
+
 
 if __name__ == "__main__":
     unittest.main()
