833b2604da1d574f2d242405a3a8aa42dd63e44e
katarzyna-winiarczyk <katarzyna.winiarczyk@xcaliber.com>
2020-04-08 21:08:52 +0200

    Parameters handover

--- test_add_group2.py
+++ test_add_group2.py
@@ -15,47 +15,51 @@ class TestAddGroup2(unittest.TestCase):
     def test_add_group2(self):
         wd = self.wd
         self.open_home_page(wd)
-        self.login(wd)
-        self.create_group(wd)
+        self.login(wd, username="admin", password="secret")
+        self.create_group(wd, name="Test2", header="Test", footer"Test2")
+        self.return_to_groups_page(wd)
+        self.logout(wd)
+
+    def test_add_empty_group2(self):
+        wd = self.wd
+        self.open_home_page(wd)
+        self.login(wd, username="admin", password="secret")
+        self.create_group(wd, name="", header="", footer"")
         self.return_to_groups_page(wd)
         self.logout(wd)
 
     def logout(self, wd):
-        # logout
         wd.find_element_by_link_text("Logout").click()
 
     def return_to_groups_page(self, wd):
-        # return to groups page
         wd.find_element_by_link_text("groups").click()
 
-    def create_group(self, wd):
+    def create_group(self, wd, name, header, footer):
         # init group creation
         wd.find_element_by_name("new").click()
         # fill group form
         wd.find_element_by_name("group_name").click()
         wd.find_element_by_name("group_name").clear()
-        wd.find_element_by_name("group_name").send_keys("Test2")
+        wd.find_element_by_name("group_name").send_keys(name)
         wd.find_element_by_name("group_header").click()
         wd.find_element_by_name("group_header").clear()
-        wd.find_element_by_name("group_header").send_keys("Test")
+        wd.find_element_by_name("group_header").send_keys(header)
         wd.find_element_by_name("group_footer").click()
         wd.find_element_by_name("group_footer").clear()
-        wd.find_element_by_name("group_footer").send_keys("Test2")
+        wd.find_element_by_name("group_footer").send_keys(footer)
         # submit group creation
         wd.find_element_by_name("submit").click()
 
-    def login(self, wd):
-        # login
+    def login(self, wd, username, password):
         wd.find_element_by_name("user").click()
         wd.find_element_by_name("user").clear()
-        wd.find_element_by_name("user").send_keys("admin")
+        wd.find_element_by_name("user").send_keys(username)
         wd.find_element_by_name("pass").click()
         wd.find_element_by_name("pass").clear()
-        wd.find_element_by_name("pass").send_keys("secret")
+        wd.find_element_by_name("pass").send_keys(password)
         wd.find_element_by_xpath("//input[@value='Login']").click()
 
     def open_home_page(self, wd):
-        # open home page
         wd.get("http://localhost:8080/addressbook/group.php")
 
     def is_element_present(self, how, what):
