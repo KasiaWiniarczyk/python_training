65c836e8d855a4bad1245f401617a4fdc43ec31e
katarzyna-winiarczyk <katarzyna.winiarczyk@xcaliber.com>
2020-04-13 19:05:04 +0200

    Creating GroupHelper class

--- test/test_add_group3.py
+++ test/test_add_group3.py
@@ -12,13 +12,13 @@ def app(request):
 
 def test_add_group2(app):
         app.session.login(username="admin", password="secret")
-        app.create_group(Group(name="Test2", header="Test", footer="Test2"))
+        app.group.create(Group(name="Test2", header="Test", footer="Test2"))
         app.session.logout()
 
 
 def test_add_empty_group2(app):
         app.session.login(username="admin", password="secret")
-        app.create_group(Group(name="", header="", footer=""))
+        app.group.create(Group(name="", header="", footer=""))
         app.session.logout()
 
 
