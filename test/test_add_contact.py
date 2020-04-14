6af4d1016b40a34a96fb5e0fa477d3d6f7e55d20
katarzyna-winiarczyk <katarzyna.winiarczyk@xcaliber.com>
2020-04-14 20:47:05 +0200

    No5

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -9,15 +9,6 @@ def app(request):
     request.addfinalizer(fixture.destroy)
     return fixture
 
-def test_add_empty_contact(app):
-        app.session.login(username="admin", password="secret")
-        app.contact.create(Contact(firstname="", middlename="", lastname="", nickname="", title="", company="",
-                                   address="", home="", mobile="", work="", fax="", email="", email2="", email3="",
-                                   homepage="", bday="-", bmonth="-", byear="", aday="-", amonth="-", address2="",
-                                   phone2="", notes=""))
-        self.app.session.logout()
-
-
 def test_add_contact(app):
         app.session.login(username="admin", password="secret")
         app.contact.create(Contact(firstname="John", middlename="Malcolm", lastname="Grey", nickname="John", title="QA", company="XCaliber", address="Compton Street", home="120745896",
@@ -26,3 +17,10 @@ def test_add_contact(app):
                                    phone2="none", notes="none"))
         app.session.logout()
 
+def test_add_empty_contact(app):
+    app.session.login(username="admin", password="secret")
+    app.contact.create(Contact(firstname="", middlename="", lastname="", nickname="", title="", company="",
+                               address="", home="", mobile="", work="", fax="", email="", email2="", email3="",
+                               homepage="", bday="-", bmonth="-", byear="", aday="-", amonth="-", address2="",
+                               phone2="", notes=""))
+    app.session.logout()
