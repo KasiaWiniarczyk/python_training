6b5e01a85709f782b9d8869e955f0b26a6f3f120
katarzyna-winiarczyk <katarzyna.winiarczyk@xcaliber.com>
2020-04-14 21:55:56 +0200

    No6

--- fixture/group.py
+++ fixture/group.py
@@ -24,3 +24,12 @@ class GroupHelper:
         # submit group creation
         wd.find_element_by_name("submit").click()
         self.return_to_groups_page()
+
+    def delete_first_group(self):
+        wd = self.app.wd
+        # select first group
+        wd.find_element_by_name("selected[]").click()
+        # submit deletion
+        wd.find_element_by_name("delete").click()
+        self.return_to_groups_page()
+
